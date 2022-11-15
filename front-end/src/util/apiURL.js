f840a46189993b0668dfc09d20ed28f4dae68b69
Anas Na <74872757+itsanasna@users.noreply.github.com>
2022-11-15 14:44:25 -0500

    changed backend url

--- front-end/src/util/apiURL.js
+++ front-end/src/util/apiURL.js
@@ -1,5 +1,5 @@
 export const apiURL = () => {
   return window.location.hostname === "localhost"
     ? "http://localhost:3333"
-    : "https://retro-backend.fly.dev";
+    : "https://retro-haven-backend.fly.dev";
 };
